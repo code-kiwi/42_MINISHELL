b1f5cff6e269ff0076a4f2a1b887aa36eec2e2d5
Benjamin Rappo <brappo@z2r6p6.42lyon.fr>
2024-04-04 09:54:37 +0200

    ajout de la fonction array_if et protection pour array_find
    array_find teste maintenant si array est NULL
    
    la fonction array_if itere un tableau et renvoie l index du premier element
    pour lequel la fonction 'condition' passee en parametre renvoie true.
    Si aucun element ne valide la fonction ou array est NULL, -1 est renvoye

--- srcs/utils/array_utils.c
+++ srcs/utils/array_utils.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 10:22:22 by brappo            #+#    #+#             */
-/*   Updated: 2024/03/28 11:26:31 by brappo           ###   ########.fr       */
+/*   Updated: 2024/04/04 09:53:04 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	array_find(void **array, bool (equal)(void *a, void *b), void *value)
 	size_t	index;
 
 	index = 0;
+	if (array == NULL)
+		return (-1);
 	while (array[index] != NULL)
 	{
 		if (equal(array[index], value) == true)
@@ -24,4 +26,20 @@ int	array_find(void **array, bool (equal)(void *a, void *b), void *value)
 		index++;
 	}
 	return (-1);
+}
+
+int	array_if(void **array, bool condition(void *elem), size_t size)
+{
+	size_t	index;
+
+	index = 0;
+	if (array == NULL)
+		return (-1);
+	while (index < size)
+	{
+		if (condition(array[index]) == true)
+			return (index);
+		index++;
+	}
+	return (index);
 }
