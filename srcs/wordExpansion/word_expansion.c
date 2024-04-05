643f3474edda3375f17274936a0210e1eef8da8b
Benjamin Rappo <brappo@z2r6p6.42lyon.fr>
2024-04-05 13:20:37 +0200

    ajout du field splitting : la fonction qui enleve les quotes, cependant la variable expansion ne marche pas

--- srcs/wordExpansion/word_expansion.c
+++ srcs/wordExpansion/word_expansion.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 14:03:37 by brappo            #+#    #+#             */
-/*   Updated: 2024/04/05 12:21:26 by brappo           ###   ########.fr       */
+/*   Updated: 2024/04/05 13:11:19 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,15 @@ ssize_t	expand_variable(char **input, size_t variable_start, bool double_quoted)
 	return (result);
 }
 
+bool	remove_quote(char *input, bool value, size_t *index)
+{
+	if (*input != '\'' && *input != '"')
+		return (value);
+	ft_memmove(input, input + 1, ft_strlen(input));
+	*index -= 1;
+	return (!value);
+}
+
 bool	expand_string(char **input)
 {
 	size_t	index;
@@ -96,10 +105,10 @@ bool	expand_string(char **input)
 	while ((*input)[index] != '\0')
 	{
 		if ((*input)[index] == '\'' && !double_quoted)
-			single_quoted = !single_quoted;
+			single_quoted = remove_quote(*input + index, single_quoted, &index);
 		else if ((*input)[index] == '\"' && !single_quoted)
-			double_quoted = !double_quoted;
-		if ((*input)[index] == '$' && !single_quoted)
+			double_quoted = remove_quote(*input + index, double_quoted, &index);
+		else if ((*input)[index] == '$' && !single_quoted)
 		{
 			variable_length = expand_variable(input, index, double_quoted);
 			if (variable_length == -1)
