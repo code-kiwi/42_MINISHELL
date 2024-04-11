41c6c7566dc9b83c600efa00fb51c55c03b30fb4
Benjamin Rappo <brappo@z4r12p5.42lyon.fr>
2024-04-11 16:46:59 +0200

    ajout de plus de tests

--- srcs/.test/wildcard_test.c
+++ srcs/.test/wildcard_test.c
@@ -6,13 +6,13 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 13:51:37 by brappo            #+#    #+#             */
-/*   Updated: 2024/04/11 16:35:07 by brappo           ###   ########.fr       */
+/*   Updated: 2024/04/11 16:38:49y brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-#define TEST_NUMBER 14
+#define TEST_NUMBER 18
 
 bool	string_equal_wildcard(char *str_wildcard, char *str_b);
 
@@ -46,25 +46,29 @@ void	get_tests(char **tests)
 	tests[25] = "includes";
 	tests[26] = "i*des*";
 	tests[27] = "includesfile";
+	tests[28] = "test sur plusieurs characteresA";
+	tests[29] = "test sur plusieurs characteresB";
+	tests[30] = "test sur plusieurs characteres";
+	tests[31] = "test sur plusieurs characteresC";
+	tests[32] = "test****s*characteres";
+	tests[33] = "test sur plusieurs characteres";
+	tests[34] = "test*sur*plusieurs*characteres";
+	tests[35] = "test sur plusieurscharacteresA";
 }
 
-int	main(void)
+int	main()
 {
-	char	*result;
-	// char	*tests[TEST_NUMBER * 2];
-	// size_t	index;
+	char	*tests[TEST_NUMBER * 2];
+	size_t	index;
 
-	// index = 0;
-	// get_tests(tests);
-	// while (index < TEST_NUMBER)
-	// {
-	// 	printf("%s equals %s ? : %s\n", tests[index * 2], tests[index * 2 + 1], string_equal_wildcard(tests[index * 2], tests[index * 2 + 1]) ? "true" : "false");
-	// 	index++;
-	// }
-	// return (0);
-	result = expand_wildcard("*");
-	printf("result : %s\n", result);
-	free(result);
+	index = 0;
+	get_tests(tests);
+	while (index < TEST_NUMBER)
+	{
+		printf("%s equals %s ? : %s\n", tests[index * 2], tests[index * 2 + 1], string_equal_wildcard(tests[index * 2], tests[index * 2 + 1]) ? "true" : "false");
+		index++;
+	}
+	return (0);
 }
 
 // int	main(int argc, char **argv)
