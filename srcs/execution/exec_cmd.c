a9511702a1124e5f27e71bcd6a8faf2e02eee310
CodeKiwiFR <madric.hottinger.101@gmail.com>
2024-04-14 00:07:10 +0200

    Adds a function to handle command execution errors

--- srcs/execution/exec_cmd.c
+++ srcs/execution/exec_cmd.c
@@ -6,11 +6,18 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:40:35 by mhotting          #+#    #+#             */
-/*   Updated: 2024/04/13 18:09:19 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/04/14 00:06:37 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	exec_cmd_error(t_minishell *shell, char *err_msg, int status)
+{
+	fd_close(STDIN_FILENO);
+	fd_close(STDOUT_FILENO);
+	handle_error(shell, err_msg, status);
+}
 
 static void	exec_cmd_extrenal(t_minishell *shell, t_node_command *cmd)
 {
@@ -19,27 +26,16 @@ static void	exec_cmd_extrenal(t_minishell *shell, t_node_command *cmd)
 
 	if (shell == NULL || cmd == NULL || !cmd->argv || cmd->argv[0] == NULL)
 		handle_error(shell, ERROR_MSG_ARGS, EXIT_FAILURE);
-	exec_cmd_handle_redirections(shell, cmd);
-	// ERROR HANDLING?
+	exec_cmd_handle_redirections(shell, cmd);	// Handle error!!!
 	command_path = exec_cmd_get_command_path(shell, (cmd->argv)[0]);
 	if (command_path == NULL)
-	{
-		fd_close(STDIN_FILENO);
-		fd_close(STDOUT_FILENO);
-		handle_error(shell, ERROR_MSG_CMD_NOTFOUND, STATUS_CMD_NOT_FOUND);
-	}
+		exec_cmd_error(shell, ERROR_MSG_CMD_NOTFOUND, STATUS_CMD_NOT_FOUND);
 	env = env_get_all_array(shell->env);
 	if (env == NULL)
-	{
-		fd_close(STDIN_FILENO);
-		fd_close(STDOUT_FILENO);
-		handle_error(shell, ERROR_MSG_CMD_NOTFOUND, STATUS_CMD_NOT_FOUND);
-	}
+		exec_cmd_error(shell, ERROR_MSG_MEM, STATUS_CMD_NOT_FOUND);
 	execve(command_path, cmd->argv, env);
 	ft_free_str_array(&env);
-	fd_close(STDIN_FILENO);
-	fd_close(STDOUT_FILENO);
-	handle_error(shell, ERROR_MSG_CMD_EXEC, STATUS_CMD_NOT_EXEC);
+	exec_cmd_error(shell, ERROR_MSG_CMD_EXEC, STATUS_CMD_NOT_EXEC);
 }
 
 void	exec_cmd(t_minishell *shell, t_node_command *cmd)
