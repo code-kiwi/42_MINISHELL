9606bda34cd87786f9ca251bb7f9c5ad13c28da1
mhotting <madric.hottinger.101@gmail.com>
2024-04-17 14:06:46 +0200

    Removes temporary code from subshell execution (TEMP_build_ast() function)

--- srcs/execution/exec_node_subshell.c
+++ srcs/execution/exec_node_subshell.c
@@ -6,144 +6,11 @@
 /*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 13:52:21 by mhotting          #+#    #+#             */
-/*   Updated: 2024/04/17 13:42:25 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/04/17 14:05:45 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/* ********************************************************************** */
-// TEMPORARY CODE
-static t_node	*node_cmd_create(char *cmd)
-{
-	t_node	*node;
-	int		argc;
-	char	**argv;
-
-	argv = ft_split(cmd, " ");
-	if (argv == NULL)
-	{
-		handle_error(NULL, "Impossible to create cmd node", 0);
-		return (NULL);
-	}
-	argc = 0;
-	while (argv[argc] != NULL)
-		argc++;
-	node = node_command_create(argc, argv);
-	if (node == NULL)
-	{
-		handle_error(NULL, "Impossible to create cmd node", 0);
-		ft_free_str_array(&argv);
-		return (NULL);
-	}
-	return (node);
-}
-
-static bool	node_cmd_add_redirs(t_node *node, char *op, char *filename)
-{
-	if (node == NULL || node->type != NODE_COMMAND)
-	{
-		errno = EINVAL;
-		handle_error(NULL, "Impossible to create cmd node", 0);
-		return (false);
-	}
-	if (!node_command_add_redirection(node, op, filename))
-	{
-		handle_error(NULL, "Creating node's redirections impossible", 0);
-		return (false);
-	}
-	return (true);
-}
-
-static t_node	*node_pip_create(t_node *left, t_node *right)
-{
-	t_node	*node_pipe;
-
-	node_pipe = node_pipe_create();
-	if (node_pipe == NULL)
-	{
-		handle_error(NULL, "Impossible to create PIPE node", 0);
-		return (NULL);
-	}
-	node_pipe->child_left = left;
-	node_pipe->child_right = right;
-	return (node_pipe);
-}
-
-static t_node	*node_and_create_test(t_node *left, t_node *right)
-{
-	t_node	*node_and;
-
-	node_and = node_and_create();
-	if (node_and == NULL)
-	{
-		handle_error(NULL, "Impossible to create AND node", 0);
-		return (NULL);
-	}
-	node_and->child_left = left;
-	node_and->child_right = right;
-	return (node_and);
-}
-
-/*
-static t_node	*node_or_create_test(t_node *left, t_node *right)
-{
-	t_node	*node_or;
-
-	node_or = node_or_create();
-	if (node_or == NULL)
-	{
-		handle_error(NULL, "Impossible to create OR node", 0);
-		return (NULL);
-	}
-	node_or->child_left = left;
-	node_or->child_right = right;
-	return (node_or);
-}
-*/
-
-static t_node	*TEMP_build_ast(t_list	*token_list)
-{
-	t_node	*node_c1;
-	t_node	*node_c2;
-	t_node	*node_c3;
-	t_node	*node_and;
-	t_node	*node_pipe;
-	(void) token_list;
-
-	// Creates node_c1
-	node_c1 = node_cmd_create("cat");
-	if (node_c1 == NULL)
-		return (NULL);
-	if (!node_cmd_add_redirs(node_c1, ">", "outfile"))
-		return (NULL);
-
-	// Creates node_c2
-	node_c2 = node_cmd_create("echo 13");
-	if (node_c2 == NULL)
-		return (NULL);
-
-	// Creates node_c3
-	node_c3 = node_cmd_create("echo 23");
-	if (node_c3 == NULL)
-		return (NULL);
-	if (!node_cmd_add_redirs(node_c3, ">", "outfile2"))
-		return (NULL);
-
-	// Creates pipe node
-	node_pipe = node_pip_create(node_c1, node_c2);
-	if (node_pipe == NULL)
-		return (NULL);
-
-	// Creates and node
-	node_and = node_and_create_test(node_pipe, node_c3);
-	if (node_and == NULL)
-		return (NULL);
-	return (node_and);
-}
-
-// END TEMPORARY CODE
-/* ********************************************************************** */
 
 /*
  *	Frees the memory, closes the given fds and exits the program with error
@@ -233,7 +100,7 @@ static void	exec_subshell(t_minishell *mainshell, t_node *node, int fds[2])
 	exec_subshell_set_fds(node_sub, fds, fds_subshell);
 	if (fds_subshell[0] == FD_ERROR || fds_subshell[1] == FD_ERROR)
 		exec_subshell_error(&subshell, fds_subshell);
-	subshell.ast = TEMP_build_ast(node_sub->token_list);
+	subshell.ast = build_ast(node_sub->token_list);
 	if (subshell.ast == NULL)
 		exec_subshell_error(&subshell, fds_subshell);
 	exec_ast(&subshell, fds_subshell);
