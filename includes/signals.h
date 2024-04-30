df83e8c9711677122c929ebd924be7a2854d5d11
root <root@BenjaminMaria>
2024-04-30 14:25:17 +0200

    norminette OK

--- includes/signals.h
+++ includes/signals.h
@@ -6,19 +6,19 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 10:11:55 by root              #+#    #+#             */
-/*   Updated: 2024/04/30 14:01:17 by root             ###   ########.fr       */
+/*   Updated: 2024/04/30 14:23:15 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNALS_H
-# define SIGANLS_H
+# define SIGNALS_H
 
 # define RL_ERROR "readline error"
 
 # include "minishell.h"
 
-void	init_signals();
+void	init_signals(void);
 void	handle_interactive_signals(t_minishell *shell);
-bool	catch_sigint();
+bool	catch_sigint(void);
 
 #endif // !SIGNALS_H
--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 10:06:42 by root              #+#    #+#             */
-/*   Updated: 2024/04/30 14:02:16 by root             ###   ########.fr       */
+/*   Updated: 2024/04/30 14:22:49 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <readline/readline.h>
 
-int	code_received;
+int	g_code_received;
 
 static void	signal_handler(int code)
 {
@@ -27,32 +27,31 @@ static void	signal_handler(int code)
 		rl_replace_line("", 1);
 		rl_redisplay();
 	}
-	if (code_received == -1)
-		code_received = code;
+	if (g_code_received == -1)
+		g_code_received = code;
 }
 
-void	init_signals()
+void	init_signals(void)
 {
-	code_received = -1;
+	g_code_received = -1;
 	signal(SIGINT, &signal_handler);
 }
 
 void	handle_interactive_signals(t_minishell *shell)
 {
-	if (code_received != SIGINT)
+	if (g_code_received != SIGINT)
 		return ;
 	t_minishell_free(shell);
 	exit(EXIT_SUCCESS);
-	code_received = -1;
+	g_code_received = -1;
 }
 
-bool	catch_sigint()
+bool	catch_sigint(void)
 {
-	if (code_received == SIGINT)
+	if (g_code_received == SIGINT)
 	{
-		code_received = -1;
+		g_code_received = -1;
 		return (true);
 	}
 	return (false);
 }
-
