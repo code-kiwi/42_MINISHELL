a9924762efdd899b04ee81d87843f21cbf590245
Benjamin Rappo <brappo@z2r6p6.42lyon.fr>
2024-05-13 10:33:15 +0200

    fix multi ligne input

--- srcs/tokenRecognition/tokenize_str.c
+++ srcs/tokenRecognition/tokenize_str.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 14:59:56 by brappo            #+#    #+#             */
-/*   Updated: 2024/05/10 17:01:50 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/05/13 10:31:57 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ t_list	*tokenize_str(char *str, t_token_parser *token_parser)
 	if (str == NULL)
 		return (NULL);
 	else if (*str == '\0' || string_contains_only_spaces(str))
-		return (get_end_token_list());
+		return (NULL);
 	skip_blank(str, &index);
 	while (str[index] != '\0')
 	{
