ded47a8121a6c9154c9b1183fc3da00dfd6ed189
Benjamin Rappo <brappo@z2r6p6.42lyon.fr>
2024-05-13 16:41:08 +0200

    ajout des tests

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: brappo <brappo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 10:14:16 by mhotting          #+#    #+#             */
-/*   Updated: 2024/05/13 14:18:15 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/05/13 16:23:23 by brappo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,46 @@
 #include "build_ast.h"
 #include "execution.h"
 #include "signals.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "token.h"
+
+void	get_enum_str(char **array)
+{
+	array[0] = "END";
+	array[1] = "WORD";
+	array[2] = "OPERATOR";
+	array[3] = "DGREAT";
+	array[4] = "DLESS";
+	array[5] = "GREAT";
+	array[6] = "LESS";
+	array[7] = "AND_IF";
+	array[8] = "OR_IF";
+	array[9] = "PIPE";
+	array[10] = "SHELL_OPEN";
+	array[11] = "SHELL_CLOSE";
+	array[12] = "ASSIGNEMENT WORD";
+}
+
+void	print_token(void *token_void)
+{
+	t_token	*token;
+	char	*enum_strs[13];
+
+	if (token_void == NULL)
+		return ;
+	token = (t_token *)token_void;
+	if (token->str != NULL)
+		printf("%s : ", token->str);
+	get_enum_str(enum_strs);
+	printf("\033[0;35m");
+	if (token->type > 12 || token->type < 0)
+		printf("ERROR");
+	else
+		printf("%s", enum_strs[token->type]);
+	printf("\033[0m");
+	fflush(stdout);
+}
 
 static void	project_init(t_minishell *shell, int argc, char **argv, char **envp)
 {
@@ -56,6 +96,7 @@ static int	project_main_loop(t_minishell *shell)
 		return (STATUS_INVALID_USE);
 	}
 	token_recognition(shell);
+	ft_lstprint(shell->tokens, print_token);
 	if (get_sigint())
 		return (STATUS_SIGINT_STOP);
 	shell->ast = build_ast(shell->tokens);
