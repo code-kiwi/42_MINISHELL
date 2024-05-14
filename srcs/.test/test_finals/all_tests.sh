0d731ec4e4029c52b9a0c8d62732f0d6e6f27021
root <root@BenjaminMaria>
2024-05-14 11:41:19 +0200

    le main attent maintenantr un input entre les tests

--- srcs/.test/test_finals/all_tests.sh
+++ srcs/.test/test_finals/all_tests.sh
@@ -15,30 +15,49 @@ fi
 
 $1 < go_wilds.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < pipe.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < redirs.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < test_expansion_full.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < test_logical.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < test_path_cmds.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < test_quote.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < test_right_expansion.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < test_simple_cmd.txt
 echo "$separator"
+read -n1
+clear
 
 $1 < test_syntax.txt
-echo "$separator
+echo "$separator"
+clear
