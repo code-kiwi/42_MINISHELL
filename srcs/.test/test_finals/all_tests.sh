4d8f482cfeb82f140160b8f167d164181beadbad
root <root@BenjaminMaria>
2024-05-14 12:19:20 +0200

    update de all_tests.sh

--- srcs/.test/test_finals/all_tests.sh
+++ srcs/.test/test_finals/all_tests.sh
@@ -3,6 +3,9 @@ if [ -z "$1" ]; then
 	exit
 fi
 
+GREEN='\033[0;32m'
+RESET_COLOR='\033[0m'
+
 separator="
 
 --------------------------------
@@ -13,51 +16,135 @@ then
 	(cd $(dirname "$1") && make) || exit
 fi
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #              go_wilds               #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < go_wilds.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #                pipe                 #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < pipe.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #                redirs               #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < redirs.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #        test_expansion_full          #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < test_expansion_full.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #            test_logical             #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < test_logical.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #          test_path_cmds             #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < test_path_cmds.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #             test_quote              #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < test_quote.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
-$1 < test_right_expansion.txt
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #        test_right_expansion         #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
+$1 < test_right_expansion_type.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #          test_simple_cmds           #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < test_simple_cmd.txt
 echo "$separator"
+tput cup 0 0
 read -n1
 clear
 
+echo -e $GREEN
+echo        " #######################################
+ #                                     #
+ #           test_syntax               #
+ #                                     #
+ #######################################"
+echo -e $RESET_COLOR
 $1 < test_syntax.txt
+echo "copy this into the shell : "
+cat test_syntax.txt
+$1
 echo "$separator"
+tput cup 0 0
+read -n1
 clear
